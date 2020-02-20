84e668ffbf2608f8bb034e803a90b1006fdd5074
dellife <seheekwon21@gmail.com>
2020-02-20 09:16:41 +0900

    HTTP 메시지 컨버터

--- src/main/java/com/dellife/SampleController.java
+++ src/main/java/com/dellife/SampleController.java
@@ -2,6 +2,7 @@ package com.dellife;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class SampleController {
     @GetMapping("/hello/{name}")
     public Person hello(@PathVariable("name") Person person) {
         return person;
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestBody String body) {
+        return "hello person";
     }
 
 }
--- src/main/java/com/dellife/WebConfig.java
+++ src/main/java/com/dellife/WebConfig.java
@@ -2,10 +2,12 @@ package com.dellife;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -26,4 +28,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .resourceChain(true);
 //                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
     }
+
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//
+//    }
+//
+//    @Override
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//
+//    }
 }
